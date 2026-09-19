7b5814c3c019f08cb979a01ed5abe07d67f7981c
xhhcn <huiw1128@icloud.com>
2026-09-19 08:25:06 +0800

    chore: agent version 1.3.22

--- client/main.go
+++ client/main.go
@@ -1214,7 +1214,7 @@ func collectSystemMetrics() metricPayload {
 		NetOutMBps:         netOut,
 		TotalNetInBytes:    totalNetInBytes,
 		TotalNetOutBytes:   totalNetOutBytes,
-		AgentVersion:       "1.3.21",
+		AgentVersion:       "1.3.22",
 		Alert:              false, // Can be enhanced with actual alert logic
 	}
 }
